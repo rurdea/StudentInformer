13fc02e9d3c21179458ecf48c58d0cb23b7187da
rurdea <radu.urdea@hotmail.com>
2015-02-08 22:28:46 +0200

    added  ckeditor; updated schedule;

--- StudentInformerWebApp/Schedule.aspx.cs
+++ StudentInformerWebApp/Schedule.aspx.cs
@@ -4,14 +4,56 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.IO;
 
 namespace StudentInformerWebApp
 {
-    public partial class Schedule : System.Web.UI.Page
+    public partial class Schedule : BaseDataPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                InitializeControls();
+                LoadSchedule();
+            }
         }
+
+        private void LoadSchedule()
+        {
+            ViewDiv.InnerHtml = File.ReadAllText(Server.MapPath("Schedule.html"));
+        }
+
+        private void InitializeControls()
+        {
+            ViewPanel.Visible = true;
+            EditPanel.Visible = false;
+            UpdateButton.Visible = UserManager.IsInRole(LoggedInUserId, "Admin") || UserManager.IsInRole(LoggedInUserId, "Professor");
+            UpdateButton.CommandArgument = "update";
+        }
+
+        protected void UpdateButton_Click(object sender, EventArgs e)
+        {
+            switch(UpdateButton.CommandArgument)
+            {
+                case "update":
+                    ViewPanel.Visible = false;
+                    EditPanel.Visible = true;
+                    UpdateButton.CommandArgument = "save";
+                    CKEditor1.Text = File.ReadAllText(Server.MapPath("Schedule.html"));
+                    break;
+                case "save":
+                    ViewPanel.Visible = true;
+                    EditPanel.Visible = false;
+                    UpdateButton.CommandArgument = "update";
+                    File.WriteAllText(Server.MapPath("Schedule.html"), CKEditor1.Text);
+                    ViewDiv.InnerHtml = CKEditor1.Text;
+                    break;
+            }
+        } 
     }
 }
--- StudentInformerWebApp/Schedule.aspx.designer.cs
+++ StudentInformerWebApp/Schedule.aspx.designer.cs
@@ -11,5 +11,50 @@ namespace StudentInformerWebApp {
     
     
     public partial class Schedule {
+        
+        /// <summary>
+        /// UpdateButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button UpdateButton;
+        
+        /// <summary>
+        /// ViewPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel ViewPanel;
+        
+        /// <summary>
+        /// ViewDiv control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl ViewDiv;
+        
+        /// <summary>
+        /// EditPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel EditPanel;
+        
+        /// <summary>
+        /// CKEditor1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::CKEditor.NET.CKEditorControl CKEditor1;
     }
 }
